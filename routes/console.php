ac7857e84b49a7f4bc666a94d26f77d614fc0e33
Степан <step8507@gmail.com>
2022-01-21 19:08:14 +0500

    Парсинг одной страницы

--- routes/console.php
+++ routes/console.php
@@ -16,6 +16,50 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
+Artisan::command('parseEkatalog', function () {
+
+    $search = 'intel+core';
+
+    $url = "https://www.e-katalog.ru/ek-list.php?search_=$search&katalog_from_search_=186";
+
+    $data = file_get_contents($url);
+
+    $dom = new DomDocument();
+    @$dom->loadHTMl($data);
+
+    $xpath = new DOMXPath($dom);
+
+    $totalProductsString = $xpath->query("//span[@class='t-g-q']")[0]->nodeValue ?? false;
+    preg_match_all('/\d+/', $totalProductsString, $matches);     // регулярное выражение, ищет числа в строке
+    $totalProducts = (int) $matches[0][0];
+
+    $divs = $xpath->query("//div[@class='model-short-div list-item--goods   ']");
+    $productsOnePage = $divs->length;
+    $pages = ceil($totalProducts / $productsOnePage); //округление в большую сторону
+    dd($pages);
+
+    foreach ($divs as $div) {
+        $a = $xpath->query("descendant::a[@class='model-short-title no-u no-u']", $div);
+        $name = $a[0]->nodeValue;
+
+        $price = 0;
+        $ranges = $xpath->query("descendant::div[@class='model-price-range']", $div);
+        if ($ranges->length == 1) {
+            foreach ($ranges[0]->childNodes as $child) {
+                if ($child->nodeName == 'a') {
+                    $price = 'от ' . $child->nodeValue;
+                }
+            }
+        }
+
+        $ranges = $xpath->query("descendant::div[@class='pr31 ib']", $div);
+        if ($ranges->length == 1) {
+            $price = $ranges[0]->nodeValue;
+        }
+        dump("$name: $price");
+    }
+});
+
 Artisan::command('massCategoriesInsert', function () {
     $categories = [
         [
