82b3c75469fb8546a0276f3e1783c412590e6f49
Степан <step8507@gmail.com>
2022-02-08 22:26:23 +0500

    test.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     @yield('title')
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/test.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
