3a60e06f8b572a8579ff6ae7415ed35c0d89b7f6
Степан <step8507@gmail.com>
2022-02-24 14:21:55 +0500

    Кнопка повторить заказ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/c77d685f5e.js" crossorigin="anonymous"></script>
     <!-- <script src="{{ asset('js/test.js') }}" defer></script> -->
 
     <!-- Fonts -->
@@ -19,7 +20,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />
+
 </head>
 <body>
     <div id="app">
