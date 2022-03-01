9f2de1386db74690a59ce2c513d8cbb61161ee61
Степан <step8507@gmail.com>
2022-03-01 17:40:10 +0500

    Что-то менял

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -9,7 +9,7 @@
 <h1>Админка</h1>
 <div class="btn-group" role="group" aria-label="Basic outlined example">
   <a role="button" class="btn btn-outline-primary" href="{{ route('adminUsers') }}">Список пользователей</a>
-  <a role="button" class="btn btn-outline-primary" href="{{ route('adminProducts') }}">Список продуктов</a>
+  <a role="button" class="btn btn-outline-primary" href="{{ route('adminProducts', 0) }}">Список продуктов</a>
   <a role="button" class="btn btn-outline-primary" href="{{ route('adminCategories') }}">Список категорий</a>
 </div>
 @endsection
